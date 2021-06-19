2a1f0fabae06acc1eea627985d07b5556539b0d1
Angel <angel.dealba.garcia@gmail.com>
2021-06-19 18:22:20 -0500

    more features

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ let type = document.getElementById("type");
 // VARIABLES DE CONTROLS
 let questions;
 let quesIndex = 0;
-
+let correct_index_answer;
 
 //FUNCIONES
 let getAPIData = (notReload) => {
     notReload.preventDefault();
     let url = `https://opentdb.com/api.php?amount=${amount.value}&category=${category.value}&
     difficulty=${difficulty.value}&type=${type.value}`;
-    
+
     fetch(url)
         .then(res => {
             return res.json();
@@ -39,19 +39,33 @@ const startGame = () => {
     document.getElementById("questionName").innerText = currentQuestion.question;
     // console.log(currentQuestion.question);
 
-    if(currentQuestion.incorrect_answers.length ==1){
+    if (currentQuestion.incorrect_answers.length == 1) {
         document.getElementById(1).innerText = "True";
         document.getElementById(2).innerText = "False";
         document.getElementById(3).style.display = "none";
         document.getElementById(4).style.display = "none";
-    }else{
+    } else {
         document.getElementById(1).style.display = "block";
-        document.getElementById(2).style.display = "block"; 
+        document.getElementById(2).style.display = "block";
         document.getElementById(3).style.display = "block";
         document.getElementById(4).style.display = "block";
-    }
+
+        correct_index_answer = Math.round(Math.random() * 4 + 1);
+        document.getElementById(correct_index_answer)
+         .innerText = currentQuestion.correct_answer;
+
+         let j = 0;
+         for (let i = 1; i <= 4; i++){
+             if(i === correct_index_answer) continue;
+             document.getElementById(i).innerText = 
+                currentQuestion.incorrect_answers[j];
+
+             j++;
+         }
+
+    };
 };
 
 // LISTENERS
-
 triviaForm.addEventListener("submit", getAPIData);
+
